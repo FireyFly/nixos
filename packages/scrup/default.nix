7325b4355deb6482affe2c865b2fa5f8b4fc004a
Jonas Höglund <firefly@firefly.nu>
2019-11-02 19:12:30 +0100

    fixup scrup

--- packages/scrup/default.nix
+++ packages/scrup/default.nix
@@ -4,6 +4,6 @@ writeShellScriptBin "scrup"
 ''
   dir=/tmp/scrot
   mkdir -p "$dir"
-  ${scrot}/bin/scrot "$@" "$dir/%Y-%m-%d_%H%M%S.png" -e '${up} $f'
+  ${scrot}/bin/scrot "$@" "$dir/%Y-%m-%d_%H%M%S.png" -e '${up}/bin/up $f'
 ''
 
