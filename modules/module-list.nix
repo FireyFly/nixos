4b3fd5bbcf9d02b16186f8729f471fa9c97aac6b
Jonas Höglund <firefly@firefly.nu>
2020-04-23 16:22:00 +0200

    biboumi: initial module implementation

--- modules/module-list.nix
+++ modules/module-list.nix
@@ -1,13 +1,19 @@
 [
   # generic support modules
   ./config/user-environment.nix
+
   # system profiles
   ./profiles/common.nix
   ./profiles/laptop.nix
+
+  # modules
+  ./biboumi.nix
+
   # shared system config
   ./user.nix
   ./xserver
   ./sdr.nix
-  # per-software config files
+
+  # per-software config
   ./herbstluftwm
 ]
