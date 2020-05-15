8e3ba4d79659b2689724190ee77fa307b79619df
Jonas Höglund <firefly@firefly.nu>
2020-05-15 22:44:44 +0200

    up: use mkScriptDerivation instead, use hagall

--- packages/up/default.nix
+++ packages/up/default.nix
@@ -1,31 +1,18 @@
-{ writeShellScriptBin, coreutils, openssh, exiftool, xclip }:
+{ stdenv, coreutils, exiftool, rsync, xclip }:
 
-writeShellScriptBin "up"
-''
-  if [ $# -lt 1 ]; then
-    echo >&2 "Usage: $0 <filename>"
-    exit 1
-  fi
+let
+  lib = import ../../lib;
 
-  dir="$(${coreutils}/bin/basename "$0")"  # target directory to upload to--'up' or 'pub'
+in lib.mkScriptDerivation {
+  name = "up";
+  src = ./up.in;
 
-  for f in "$@"; do
-    target="$(${coreutils}/bin/basename "$f")"
+  host = "hagall";
+  hostLocation = "/var/www/up.firefly.nu";
+  clipboardPrefix = "https://up.firefly.nu/";
 
-    # Preprocessing
-    case "$f" in
-      *.jpg)  # Strip GPS metadata
-        cp "$f" /tmp/"$target"
-        f=/tmp/"$target"
-        ${exiftool}/bin/exiftool -q -gps:all= -xmp:all= "$f"
-        ;;
-    esac
-
-    # Upload
-    path="$dir/$target"
-    ${openssh}/bin/scp -r "$f" fe:srv/http/"$path"
-  done
-
-  ${xclip}/bin/xclip -i <<<"http://xen.firefly.nu/$path"
-  ${xclip}/bin/xclip -selection clipboard -i <<<"http://xen.firefly.nu/$path"
-''
+  basename = "${coreutils}/bin/basename";
+  exiftool = "${exiftool}/bin/exiftool";
+  rsync = "${rsync}/bin/rsync";
+  xclip = "${xclip}/bin/xclip";
+}
