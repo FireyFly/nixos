8e41902efb134b03da0180bfb498f119556f2fe6
Jonas Höglund <firefly@firefly.nu>
2020-10-18 23:44:07 +0200

    hikari: unpin package

--- overlays/pins.nix
+++ overlays/pins.nix
@@ -1,14 +1,5 @@
 
 self: super: {
-  hikari = super.hikari.overrideAttrs ({ pname, ... }: {
-    version = "dev";
-    src = super.fetchdarcs {
-      url = "https://hub.darcs.net/raichoo/${pname}";
-      hash = "4a353f9db2deb9a8448454e57466e05127ccfbeb";
-      sha256 = "1szkqskzc9i3la792sr3ljk0bplj22vlf3qmlw852dd9mskzm8x0";
-    };
-  });
-
   poezio = super.poezio.overrideAttrs ({ pname, ... }: {
     version = "0.13-dev";
     src = super.fetchFromGitHub {
